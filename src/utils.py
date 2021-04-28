cbe5aef666408ee1a34c754154582a21f0320d94
hunmin-hub <hakddal707@naver.com>
2021-04-28 23:25:35 +0900

    [Modify] Hard Aug / print CV

--- src/utils.py
+++ src/utils.py
@@ -1,34 +1,74 @@
 # https://github.com/wkentaro/pytorch-fcn/blob/master/torchfcn/utils.py
 import numpy as np
 
-
 def _fast_hist(label_true, label_pred, n_class):
     mask = (label_true >= 0) & (label_true < n_class)
-    hist = np.bincount(
-        n_class * label_true[mask].astype(int) +
-        label_pred[mask], minlength=n_class ** 2).reshape(n_class, n_class)
+    hist = np.bincount(n_class * label_true[mask].astype(int) + label_pred[mask],
+                        minlength=n_class ** 2).reshape(n_class, n_class)
     return hist
 
-
-def label_accuracy_score(label_trues, label_preds, n_class):
-    """Returns accuracy score evaluation result.
-      - overall accuracy
-      - mean accuracy
-      - mean IU
-      - fwavacc
+def label_accuracy_score(hist):
     """
-    hist = np.zeros((n_class, n_class))
-    for lt, lp in zip(label_trues, label_preds):
-        hist += _fast_hist(lt.flatten(), lp.flatten(), n_class)
+    Returns accuracy score evaluation result.
+      - [acc]: overall accuracy
+      - [acc_cls]: mean accuracy
+      - [mean_iu]: mean IU
+      - [fwavacc]: fwavacc
+    """
     acc = np.diag(hist).sum() / hist.sum()
     with np.errstate(divide='ignore', invalid='ignore'):
         acc_cls = np.diag(hist) / hist.sum(axis=1)
     acc_cls = np.nanmean(acc_cls)
+
     with np.errstate(divide='ignore', invalid='ignore'):
-        iu = np.diag(hist) / (
-            hist.sum(axis=1) + hist.sum(axis=0) - np.diag(hist)
-        )
+        iu = np.diag(hist) / (hist.sum(axis=1) + hist.sum(axis=0) - np.diag(hist))
     mean_iu = np.nanmean(iu)
+
     freq = hist.sum(axis=1) / hist.sum()
     fwavacc = (freq[freq > 0] * iu[freq > 0]).sum()
-    return acc, acc_cls, mean_iu, fwavacc
+    #return acc, acc_cls, mean_iu, fwavacc
+    return mean_iu, iu
+
+
+def add_hist(hist, label_trues, label_preds, n_class):
+    """
+        stack hist(confusion matrix)
+    """
+    for lt, lp in zip(label_trues, label_preds):
+        hist += _fast_hist(lt.flatten(), lp.flatten(), n_class)
+    return hist
+
+# EarlyStopping
+class EarlyStopping:
+    def __init__(self, patience=10, verbose=False, delta=0, path='./saved/checkpoint.pt'):
+        self.patience = patience
+        self.verbose = verbose
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.val_auc_max = np.Inf
+        self.delta = delta
+        self.path = path
+
+    def __call__(self, val_loss, model):
+
+        score = val_loss
+        if self.best_score is None:
+            self.best_score = score
+            self.save_checkpoint(self.best_score, model)
+        elif score > self.best_score + self.delta:
+            self.counter += 1
+            if self.verbose:
+                print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.save_checkpoint(self.best_score, model)
+            self.counter = 0
+
+    def save_checkpoint(self, val_loss, model):
+        if self.verbose:
+            print(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
+        torch.save(model.state_dict(), self.path)
+        self.val_loss_min = val_loss
