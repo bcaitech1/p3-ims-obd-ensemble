2e2e3a02d62e0de664f6e316524410bb43764d84
happyBeagle <mj.jo.sw@gmail.com>
2021-04-30 13:19:43 +0000

    [modify file] run.py: add mode

--- run.py
+++ run.py
@@ -51,8 +51,8 @@ if __name__=="__main__":
     print(torch.cuda.get_device_name(0))
     print(torch.cuda.device_count())
 
-    if_train = False # True: train model False test model
-
+    if_train = True # True: train model False test model
+    only_valid = False # True: Not train, False: train + valid
     batch_size = 1   # Mini-batch size
     num_epochs = 150
     learning_rate = 0.0001
@@ -80,14 +80,18 @@ if __name__=="__main__":
         gc.collect()
         torch.cuda.empty_cache()
     
-        model = get_seg_model("/opt/ml/input/pretrained/hrnet_best_model(pretrained)_light_5_miou.pt", use_pretrain=True)
+        model = get_seg_model("/opt/ml/input/pretrained/hrnet_best_model(pretrained)_light_13_loss.pt", use_pretrain=False)
         model = model.cuda()
     
         criterion = nn.CrossEntropyLoss()
         optimizer = torch.optim.SGD(model.parameters(), lr=1e-4)
         val_every = 1
         saved_dir = '/opt/ml/input/pretrained'
-        train(num_epochs, model, train_loader, val_loader, criterion, optimizer, saved_dir, val_every)
+        if only_valid:
+            avrg_loss, miou, class_mIoU = validation(0, model, val_loader, criterion, 12)
+            show_class_mIoU(class_mIoU)
+        else:
+            train(num_epochs, model, train_loader, val_loader, criterion, optimizer, saved_dir, val_every, 12)
 
     else:
         test_path = dataset_path + '/test.json'
@@ -98,7 +102,7 @@ if __name__=="__main__":
                                           num_workers=0,
                                           collate_fn=collate_fn)
 
-        model = get_seg_model("/opt/ml/input/pretrained/hrnet_best_model(pretrained)_light_5_miou.pt", use_pretrain=True)
+        model = get_seg_model("/opt/ml/input/pretrained/hrnet_best_model(pretrained)_light_13_loss.pt", use_pretrain=True)
         model = model.cuda()
         file_names, preds = test(model, test_loader)
-        save_sub(file_names, preds, "/opt/ml/code/submission/hrnet_best_miou_epoch_5.csv")
+        save_sub(file_names, preds, "/opt/ml/code/submission/hrnet_best_miou_epoch_13.csv")
