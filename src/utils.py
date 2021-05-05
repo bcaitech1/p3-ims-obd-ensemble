f3b59be3ae75da964ebb7cc11ef51b9d5264417d
hunmin-hub <hakddal707@naver.com>
2021-05-05 20:04:18 +0900

    [Modify] utils.py
    기능추가

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,11 @@
 # https://github.com/wkentaro/pytorch-fcn/blob/master/torchfcn/utils.py
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.modules.loss import _WeightedLoss
 import numpy as np
+import math
+from torch.optim.lr_scheduler import _LRScheduler
 
 def _fast_hist(label_true, label_pred, n_class):
     mask = (label_true >= 0) & (label_true < n_class)
@@ -7,6 +13,23 @@ def _fast_hist(label_true, label_pred, n_class):
                         minlength=n_class ** 2).reshape(n_class, n_class)
     return hist
 
+def public_label_accuracy_score(label_trues, label_preds, n_class):
+    hist = np.zeros((n_class, n_class))
+    for lt, lp in zip(label_trues, label_preds):
+        hist += _fast_hist(lt.flatten(), lp.flatten(), n_class)
+    acc = np.diag(hist).sum() / hist.sum()
+    with np.errstate(divide='ignore', invalid='ignore'):
+        acc_cls = np.diag(hist) / hist.sum(axis=1)
+    acc_cls = np.nanmean(acc_cls)
+    with np.errstate(divide='ignore', invalid='ignore'):
+        iu = np.diag(hist) / (
+            hist.sum(axis=1) + hist.sum(axis=0) - np.diag(hist)
+        )
+    mean_iu = np.nanmean(iu)
+    freq = hist.sum(axis=1) / hist.sum()
+    fwavacc = (freq[freq > 0] * iu[freq > 0]).sum()
+    return mean_iu
+    
 def label_accuracy_score(hist):
     """
     Returns accuracy score evaluation result.
@@ -40,23 +63,22 @@ def add_hist(hist, label_trues, label_preds, n_class):
 
 # EarlyStopping
 class EarlyStopping:
-    def __init__(self, patience=10, verbose=False, delta=0, path='./saved/checkpoint.pt'):
+    def __init__(self, patience=3, verbose=False, delta=0, path='./saved/checkpoint.pt'):
         self.patience = patience
         self.verbose = verbose
         self.counter = 0
         self.best_score = None
         self.early_stop = False
-        self.val_auc_max = np.Inf
+        self.val_iou_max = 0
         self.delta = delta
         self.path = path
 
-    def __call__(self, val_loss, model):
-
-        score = val_loss
+    def __call__(self, val_iou, model):
+        score = val_iou
         if self.best_score is None:
             self.best_score = score
             self.save_checkpoint(self.best_score, model)
-        elif score > self.best_score + self.delta:
+        elif score < self.best_score + self.delta:
             self.counter += 1
             if self.verbose:
                 print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
@@ -67,8 +89,104 @@ class EarlyStopping:
             self.save_checkpoint(self.best_score, model)
             self.counter = 0
 
-    def save_checkpoint(self, val_loss, model):
+    def save_checkpoint(self, val_iou, model):
         if self.verbose:
-            print(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
+            print(f'Validation mIoU increased ({self.val_iou_max:.6f} --> {val_loss:.6f}).  Saving model ...')
         torch.save(model.state_dict(), self.path)
-        self.val_loss_min = val_loss
+        self.val_iou_max = val_iou
+
+
+class CosineAnnealingWarmUpRestarts(_LRScheduler):
+    def __init__(self, optimizer, T_0, T_mult=1, eta_max=0.1, T_up=0, gamma=1., last_epoch=-1):
+        self.T_0 = T_0
+        self.T_mult = T_mult
+        self.base_eta_max = eta_max
+        self.eta_max = eta_max
+        self.T_up = T_up
+        self.T_i = T_0
+        self.gamma = gamma
+        self.cycle = 0
+        self.T_cur = last_epoch
+        super(CosineAnnealingWarmUpRestarts, self).__init__(optimizer, last_epoch)
+    
+    def get_lr(self):
+        if self.T_cur == -1:
+            return self.base_lrs
+        elif self.T_cur < self.T_up:
+            return [(self.eta_max - base_lr)*self.T_cur / self.T_up + base_lr for base_lr in self.base_lrs]
+        else:
+            return [base_lr + (self.eta_max - base_lr) * (1 + math.cos(math.pi * (self.T_cur-self.T_up) / (self.T_i - self.T_up))) / 2
+                    for base_lr in self.base_lrs]
+
+    def step(self, epoch=None):
+        if epoch is None:
+            epoch = self.last_epoch + 1
+            self.T_cur = self.T_cur + 1
+            if self.T_cur >= self.T_i:
+                self.cycle += 1
+                self.T_cur = self.T_cur - self.T_i
+                self.T_i = (self.T_i - self.T_up) * self.T_mult + self.T_up
+        else:
+            if epoch >= self.T_0:
+                if self.T_mult == 1:
+                    self.T_cur = epoch % self.T_0
+                    self.cycle = epoch // self.T_0
+                else:
+                    n = int(math.log((epoch / self.T_0 * (self.T_mult - 1) + 1), self.T_mult))
+                    self.cycle = n
+                    self.T_cur = epoch - self.T_0 * (self.T_mult ** n - 1) / (self.T_mult - 1)
+                    self.T_i = self.T_0 * self.T_mult ** (n)
+            else:
+                self.T_i = self.T_0
+                self.T_cur = epoch
+                
+        self.eta_max = self.base_eta_max * (self.gamma**self.cycle)
+        self.last_epoch = math.floor(epoch)
+        for param_group, lr in zip(self.optimizer.param_groups, self.get_lr()):
+            param_group['lr'] = lr
+            
+########### LOSS FUNCTION ################
+def diceCoeffv2(pred, gt, eps=1e-5, activation='softmax2d'):
+    """ computational formula：
+        dice = (2 * tp) / (2 * tp + fp + fn)
+    """
+ 
+    if activation is None or activation == "none":
+        activation_fn = lambda x: x
+    elif activation == "sigmoid":
+        activation_fn = nn.Sigmoid()
+    elif activation == "softmax2d":
+        activation_fn = nn.Softmax2d()
+    else:
+        raise NotImplementedError("Activation implemented for sigmoid and softmax2d activation function operation")
+ 
+    pred = activation_fn(pred)
+    # gt -> one-hot encoding
+    gt = F.one_hot(gt, num_classes = 12).permute(0,3,1,2).contiguous()
+    N = gt.size(0)
+    pred_flat = pred.view(N, -1)
+    gt_flat = gt.view(N, -1)
+ 
+    tp = torch.sum(gt_flat * pred_flat, dim=1)
+    fp = torch.sum(pred_flat, dim=1) - tp
+    fn = torch.sum(gt_flat, dim=1) - tp
+    loss = (2 * tp + eps) / (2 * tp + fp + fn + eps)
+    return loss.sum() / N
+
+class DiceScore(nn.Module):
+    __name__ = 'dice_score'
+    def __init__(self, activation='softmax2d'):
+        super(DiceScore, self).__init__()
+        self.activation = activation
+ 
+    def forward(self, y_pr, y_gt):
+        return diceCoeffv2(y_pr, y_gt, activation=self.activation)
+    
+class DiceCELoss(nn.Module):
+    def __init__(self):
+        super(DiceCELoss, self).__init__()
+        self.DiceScore=DiceScore()
+        self.CELoss=nn.CrossEntropyLoss()
+    
+    def forward(self,y_pr,y_gt):
+        return self.CELoss(y_pr,y_gt)-torch.log(self.DiceScore(y_pr,y_gt))
